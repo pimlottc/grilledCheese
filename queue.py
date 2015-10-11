44ca05f012ae4b47f8f861d68d4faa8ba881c341
JACOB R. GARCOWSKI <jgarcows@us.ibm.com>
2015-10-10 22:33:09 -0400

    Input playing

--- queue.py
+++ queue.py
@@ -93,13 +93,13 @@ print '<FORM method="post" action="/cgi-bin/grilledCheese/order.py">'
 print '<table>\n <tr>\n  <th>Name</th>\n  <th>Bread</th>\n  <th>Cheese (up to 2)</th>\n  <th>Toppings</th>\n </tr>'
 print ' <tr>\n  <td>\n   <input type="text" name="name">\n  </td>'
 print '  <td>'
-twoColumnCheckTable("cheese", cheeses)
+columnCheckTable("cheese", cheeses)
 print '  </td>'
 print '  <td>'
-twoColumnCheckTable("bread", breads)
+columnCheckTable("bread", breads)
 print '  </td>'
 print '  <td>'
-twoColumnCheckTable("toppings", toppings)
+columnCheckTable("toppings", toppings)
 print '  </td>'
 print '</body>'
 
